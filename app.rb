6059f1012e463ddb9ab6dd1cac2078ad213542c0
“jzhouerm” <jzhouerm@hotmail.comgit config --global user.name jzhouermgit config --global user.email jzhouerm@hotmail.com>
2020-07-13 19:46:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,24 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		"<h1>Hello World</h1>"
+		erb :index
 	end
+
+	# get '/' do
+	# 	erb :index
+	# end
+
+	# get '/info' do
+	# 	"Testing the info page"
+	# end
+
+	get '/' do
+		erb :index
+	end
+
+	get "/info" do
+		erb :info
+	end
+
 end
